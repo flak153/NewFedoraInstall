8a280f3667514073235f31e8d3f31341f8cb3595
Flak <flak@localhost.localdomain>
2018-02-28 22:47:56 -0500

    atom too big

--- .atom/packages/ide-haskell-hoogle/src/atom-config.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace AtomTypes {
-  interface ConfigInterface {
-      'ide-haskell-hoogle.hoogleType': '' | 'http://hoogle.haskell.org/' | 'https://haskell.org/hoogle/'
-      'ide-haskell-hoogle.hooglePath': string
-      'ide-haskell-hoogle.webZoomFactor': number
-  }
-}
