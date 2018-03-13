9ddf1399f43d3465c91ec1cd74b4ca6c644b070c
flak <flak@localhost.localdomain>
2018-03-13 07:58:04 -0400

    fixes

--- Scripts/1_repos.sh
+++ Scripts/1_repos.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #RPM Fusion Repo
-dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 #Nvidia Repo from Negativo17
 dnf config-manager --add-repo=https://negativo17.org/repos/fedora-nvidia.repo
@@ -12,3 +12,4 @@ dnf config-manager --add-repo=https://negativo17.org/repos/fedora-spotify.repo
 dnf copr enable -y heliocastro/hack-fonts
 
 dnf update -y
+reboot
